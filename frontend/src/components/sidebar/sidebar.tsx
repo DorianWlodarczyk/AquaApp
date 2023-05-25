76643e80eb1c843c99adcc5d55b206e0488b3efc
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-25 21:00:56 +0200

    Add footer to sidebar

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
-export 
+export interface SidebarProps {
+  headerName?: String,
+}
 
-const Sidebar = () => {
+const Sidebar = ({headerName}: SidebarProps) => {
+
+  const Header = () => {
+    return (
+      <div>header</div>
+    )
+  }
+
+  const Footer = () => {
+    return(
+      <div className='h-[50px] bg-neutral-700 flex flex-col justify-center'>
+        <div className='h-[40px] w-[40px] bg-neutral-500 rounded-md ml-auto mr-2 cursor-pointer select-none'>
+          <div className='h-full flex justify-center items-center text-xl'>
+            {"<"}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>sidebar</div>
+    <div className='bg-neutral-900 w-[200px] h-[100vh] flex flex-col justify-between'>
+      <Header/>
+      <Footer/>
+    </div>
   )
 }
 
 export default Sidebar
+
