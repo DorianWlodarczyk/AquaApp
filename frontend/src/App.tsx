25b8461eca523be9e6a9445e531d3d41becf11be
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-25 23:14:11 +0200

    Render button in sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import Sidebar from './components/sidebar/sidebar';
+import Sidebar, { Category } from './components/sidebar/sidebar';
+
+
+const categories: Category[] = [
+  {
+    headerName: 'Kategoria 1',
+    buttons: [
+      {
+        name: 'Jeden'
+      },
+      {
+        name: 'Dwa'
+      },
+      {
+        name: 'Trzy'
+      }
+    ]
+  },
+  {
+    headerName: 'Kategoria 2',
+    buttons: [
+      {
+        name: 'Jeden'
+      },
+      {
+        name: 'Dwa'
+      },
+      {
+        name: 'Trzy'
+      }
+    ]
+  }
+];
+
 
 function App() {
   return (
     <>
-      <Sidebar />
+      <Sidebar categories={categories}/>
     </>
   );
 }
--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,42 +1,81 @@
 import React from 'react'
-import SidebarButton from './sidebar-button/sidebar-button'
+import SidebarButton, { SidebarButtonProps } from './sidebar-button/sidebar-button'
 
-export interface SidebarProps {
-  headerName?: String,
+export interface Category {
+    headerName?: string,
+    buttons?: SidebarButtonProps[]
+}
+interface props {
+  categories: Category[],
 }
 
-const Sidebar = ({headerName}: SidebarProps) => {
-
-  const Header = () => {
-    return (
-      <div>
-        <div className='w-full h-[50px] flex items-center justify-center flex-col'>
-          <div className='text-white text-xl '>
-            AQUA FRIENDS
+const renderButtons = (buttons?: SidebarButtonProps[]) => {
+  return (
+    <>
+      {buttons?.map((item: SidebarButtonProps, index) => {
+        return(
+          <div key={index}>
+            <SidebarButton
+              name={item.name}
+            />
           </div>
-        </div>
-        <hr className='mx-3 h-[2px] border-0 bg-gradient-to-r from-transparent via-neutral-500 to-transparent' />
-      </div>
+        )
+      })}
+    </>
+  )
+}
 
-    )
-  }
-
-  const Footer = () => {
-    return(
-      <div className='h-[50px] bg-neutral-700 flex flex-col justify-center'>
-        <div className='h-[40px] w-[40px] bg-neutral-500 rounded-md ml-auto mr-2 cursor-pointer select-none'>
-          <div className='h-full flex justify-center items-center text-xl'>
-            {"<"}
+const Categories = ({categories}: props) => {
+  return (
+    <>
+      {categories.map((item: Category, index) => {
+        return (
+          <div key={index} className='text-neutral-500 font-medium pl-5 mt-5 text-sm'>
+            <div className='mb-2'>
+              {item.headerName}
+            </div>
+            <div className=''>
+              {renderButtons(item.buttons)}
+            </div>
           </div>
+        )
+      })}
+    </>
+  )
+}
+
+const Header = () => {
+  return (
+    <div>
+      <div className='w-full h-[50px] flex items-center justify-center flex-col'>
+        <div className='text-white text-xl '>
+          AQUA FRIENDS
         </div>
       </div>
-    )
-  }
+      <hr className='mx-3 h-[2px] border-0 bg-gradient-to-r from-transparent via-neutral-500 to-transparent' />
+    </div>
+  )
+}
 
+const Footer = () => {
+  return(
+    <div className='h-[50px] bg-neutral-700 flex flex-col justify-center'>
+      <div className='h-[40px] w-[40px] bg-neutral-500 rounded-md ml-auto mr-2 cursor-pointer select-none'>
+        <div className='h-full flex justify-center items-center text-xl'>
+          {"<"}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+const Sidebar = ({categories}: props) => {
   return (
     <div className='bg-neutral-900 w-[225px] h-[100vh] flex flex-col justify-between'>
       <div>
         <Header/>
+        <Categories categories={categories}/>
       </div>
       <Footer/>
     </div>
