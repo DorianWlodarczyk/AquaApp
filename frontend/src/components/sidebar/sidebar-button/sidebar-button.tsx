ae06b64c9ea13431f56d4b53a101ac5d29e3ef12
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-25 22:11:51 +0200

    Create SidebarButton component

---
+++ frontend/src/components/sidebar/sidebar-button/sidebar-button.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SvgIconProps } from '@mui/material';
+import { Link } from "react-router-dom";
+
+export interface SidebarButtonProps {
+  icon?: React.ReactElement<SvgIconProps>,
+  name: string,
+  to?: string
+}
+
+const SidebarButton = ({icon, name, to}: SidebarButtonProps) => {
+  return (
+    <Link to={to ? to : "/"}>
+      <div className='w-11/12 h-[40px] flex flex-row items-center hover:bg-neutral-800 cursor-pointer rounded'>
+        <div className='text-neutral-400 pl-3 font-medium'>
+          {icon ? icon : name[0]}
+        </div>
+
+        <div className='text-neutral-400 pl-3 font-medium'>
+          {name}
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default SidebarButton
