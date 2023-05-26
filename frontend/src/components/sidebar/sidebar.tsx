dba8900dd3d0c196185ab48a830140e90d7f1ba7
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-26 03:33:53 +0200

    Reorganize sidebar component

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -3,84 +3,89 @@ import SidebarButton, { SidebarButtonProps } from './sidebar-button/sidebar-butt
 import { MainContext } from '../../contexts/main-context'
 
 export interface Category {
-    headerName?: string,
-    buttons?: SidebarButtonProps[]
+  headerName?: string,
+  buttons?: SidebarButtonProps[]
 }
 interface props {
   categories: Category[],
 }
 
-const renderButtons = (buttons?: SidebarButtonProps[]) => {
-  return (
-    <>
-      {buttons?.map((item: SidebarButtonProps, index) => {
-        return(
-          <div key={index}>
-            <SidebarButton
-              name={item.name}
-            />
-          </div>
-        )
-      })}
-    </>
-  )
-}
+const Sidebar = ({ categories }: props) => {
+  const mainContext = useContext(MainContext);
 
-const Categories = ({categories}: props) => {
-  return (
-    <>
-      {categories.map((item: Category, index) => {
-        return (
-          <div key={index} className='text-neutral-500 font-medium pl-5 mt-5 text-sm'>
-            <div className='mb-2'>
-              {item.headerName}
+  const renderButtons = (buttons?: SidebarButtonProps[]) => {
+    return (
+      <>
+        {buttons?.map((item: SidebarButtonProps, index) => {
+          return (
+            <div key={index} className=''>
+              <SidebarButton
+                name={item.name}
+              />
             </div>
-            <div className=''>
-              {renderButtons(item.buttons)}
+          )
+        })}
+      </>
+    )
+  }
+
+  const Categories = ({ categories }: props) => {
+    return (
+      <>
+        {categories.map((item: Category, index) => {
+          return (
+            <div key={index} className='text-neutral-500 font-medium mt-5 text-sm' style={{
+              paddingLeft: mainContext.isOpen ? '10px' : '0px'
+            }}>
+              <div className='mb-2'>
+                {mainContext.isOpen ? item.headerName : ''}
+              </div>
+              <div className=''>
+                {renderButtons(item.buttons)}
+              </div>
             </div>
-          </div>
-        )
-      })}
-    </>
-  )
-}
+          )
+        })}
+      </>
+    )
+  }
 
-const Header = () => {
-  return (
-    <div>
-      <div className='w-full h-[50px] flex items-center justify-center flex-col'>
-        <div className='text-white text-xl '>
-          AQUA FRIENDS
-        </div>
-      </div>
-      <hr className='mx-3 h-[2px] border-0 bg-gradient-to-r from-transparent via-neutral-500 to-transparent' />
-    </div>
-  )
-}
-
-const Footer = () => {
-const mainContext = useContext(MainContext);
-
-  return(
-    <div className='h-[50px] bg-neutral-700 flex flex-col justify-center'>
-      <div className='h-[40px] w-[40px] bg-neutral-500 rounded-md ml-auto mr-2 cursor-pointer select-none'>
-        <div className='h-full flex justify-center items-center text-xl' onClick={() => mainContext.setOpen(!mainContext.isOpen)}>
-          {"<"}
-        </div>
-      </div>
-    </div>
-  )
-}
-
-
-const Sidebar = ({categories}: props) => {
-  return (
-    <div className='bg-neutral-900 w-[225px] h-[100vh] flex flex-col justify-between'>
+  const Header = () => {
+    return (
       <div>
-        <Header/>
-        <Categories categories={categories}/>
+        <div className='w-full h-[50px] flex items-center justify-center flex-col'>
+          <div className='text-white text-center' style={{
+            fontSize: mainContext.isOpen ? '1.25rem' : '0.7rem'
+          }}>
+            AQUA FRIENDS
+          </div>
+        </div>
+        <hr className='mx-3 h-[2px] border-0 bg-gradient-to-r from-transparent via-neutral-500 to-transparent' />
       </div>
-      <Footer/>
+    )
+  }
+
+  const Footer = () => {
+    return (
+      <div className='h-[50px] bg-neutral-700 flex flex-col justify-center'>
+        <div className='h-[40px] w-[40px] bg-neutral-500 rounded-md ml-auto mr-2 cursor-pointer select-none'>
+          <div className='h-full flex justify-center items-center text-xl' onClick={() => mainContext.setOpen(!mainContext.isOpen)}>
+            {"<"}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className={`bg-neutral-900 h-[100vh] flex flex-col justify-between duration-100`} style={{
+      width: mainContext.isOpen ? '225px' : '55px'
+    }}>
+      <div>
+        <Header />
+        <Categories categories={categories} />
+      </div>
+      <Footer />
     </div>
   )
 }
