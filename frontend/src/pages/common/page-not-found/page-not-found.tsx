c9af42486ddad0ba84c7a5680a59bfc15ea219dd
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-27 00:02:08 +0200

    Create PageNotFound

--- frontend/src/pages/common/page-not-found/page-not-found.tsx
+++ frontend/src/pages/common/page-not-found/page-not-found.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import NotListedLocationIcon from '@mui/icons-material/NotListedLocation';
 
 const PageNotFound = () => {
   return (
-    <div>page-not-found</div>
+    <div className='flex h-5/6 justify-center items-center flex-col select-none text-neutral-400'>
+      <NotListedLocationIcon className='' style={{
+        fontSize: '192px'
+      }} />
+      <div className='mt-[20px] text-xl'>Nie znaleziono strony</div>
+    </div>
   )
 }
 
