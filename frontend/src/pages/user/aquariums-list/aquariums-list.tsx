58b7fb3ff52cd4bccd47b857e684475a9ce9fa5f
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-27 19:33:02 +0200

    Create Aquarium list page and add button new

--- frontend/src/pages/user/aquariums-list/aquariums-list.tsx
+++ frontend/src/pages/user/aquariums-list/aquariums-list.tsx
@@ -1,11 +1,18 @@
 // import React, { useState } from 'react'
 import EmptyAquariumsList from './empty-list/empty-aquariums-list';
-
+import AddIcon from '@mui/icons-material/Add';
 const AquariumsList = () => {
   // const [aa, setAA] = useState(true);
 
+  if (false)
+    return (<EmptyAquariumsList />)
+
   return (
-    <EmptyAquariumsList />
+    <>
+      <button className='w-[64px] h-[64px] bg-sky-600 hover:bg-sky-500 fixed right-[5%] bottom-[5%] rounded-full flex items-center justify-center shadow-lg duration-100'>
+        <AddIcon className='text-white' style={{ fontSize: '36px' }} />
+      </button>
+    </>
   )
 }
 
