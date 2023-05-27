10ea0b08260f128e2cec056fb750111be1126941
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-27 22:06:46 +0200

    Update screen brakpoints

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '425px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
