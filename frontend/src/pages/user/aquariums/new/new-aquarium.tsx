375317c276125c9b44f1488163648aa6e4136c45
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-29 15:12:09 +0200

    Create New Aquarium page

---
+++ frontend/src/pages/user/aquariums/new/new-aquarium.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewAquarium = () => {
+  return <div>NewAquarium</div>;
+};
+
+export default NewAquarium;
--- frontend/src/routes/user-routes.tsx
+++ frontend/src/routes/user-routes.tsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import TestPage from "../pages/common/test-page/test-page";
 import PageNotFound from "../pages/common/page-not-found/page-not-found";
 import AquariumsList from "../pages/user/aquariums/index/list";
+import NewAquarium from "../pages/user/aquariums/new/new-aquarium";
 
 const UserRoutes = () => {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<></>} />
-        <Route path="/aqua" element={<AquariumsList />} />
+        <Route path="aqua">
+          <Route index element={<AquariumsList />} />
+          <Route path="new" element={<NewAquarium />} />
+        </Route>
         <Route path="/test1" element={<TestPage />} />
       </Route>
+
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
