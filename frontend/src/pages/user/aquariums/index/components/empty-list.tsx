515c3d015448a2c57c28df06f65b3042f116c59e
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-29 02:59:43 +0200

    Move empty list page to other folder and change name

--- frontend/src/pages/user/aquariums/index/components/empty-list.tsx
+++ frontend/src/pages/user/aquariums/index/components/empty-list.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../../../../components/button/button'
+import Button from '../../../../../components/button/button'
 
 const EmptyAquariumsList = () => {
   return (
