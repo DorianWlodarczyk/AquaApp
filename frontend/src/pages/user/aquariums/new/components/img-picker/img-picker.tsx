617ec6fd4db844c92271e4394c93b5efad3b654f
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-30 02:07:45 +0200

    Create img-picker in NewAquarium

---
+++ frontend/src/pages/user/aquariums/new/components/img-picker/img-picker.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { aquariumImg } from "../../../../../../utils/images/aquarium-image";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+interface props {
+  previewNumber?: number;
+  onClick: (value: string) => void;
+}
+
+const ImgPicker = ({ previewNumber = 2, onClick }: props) => {
+  const [index, setIndex] = useState(0);
+
+  const getPreviewImg = (number: number) => {
+    const mapKeys: string[] = Array.from(aquariumImg.keys());
+    const output: string[] = [];
+
+    for (let i = -number; i <= number; i++) {
+      const key =
+        (index - i) % mapKeys.length >= 0
+          ? (index - i) % mapKeys.length
+          : mapKeys.length - Math.abs((index - i) % mapKeys.length);
+
+      output.push(aquariumImg.get(mapKeys[key]) || "");
+    }
+
+    return output;
+  };
+
+  useEffect(() => {
+    const mapKeys: string[] = Array.from(aquariumImg.keys());
+
+    const key =
+      index % mapKeys.length >= 0
+        ? index % mapKeys.length
+        : mapKeys.length - Math.abs(index % mapKeys.length);
+
+    onClick(aquariumImg.get(mapKeys[key]) || "");
+  }, [index, onClick]);
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-row justify-around">
+        <button
+          className="group flex w-full items-center justify-center"
+          onClick={() => setIndex(index + 1)}
+        >
+          <div className="mr-2 flex h-[60px] w-[30px] items-center justify-center rounded-full text-3xl font-bold text-neutral-600 group-hover:bg-neutral-100">
+            <ArrowBackIosNewIcon />
+          </div>
+        </button>
+
+        <img
+          className="h-[200px] w-[200px]"
+          src={getPreviewImg(previewNumber)[previewNumber]}
+          alt="Aquarium"
+        />
+
+        <button
+          className="group flex w-full items-center justify-center"
+          onClick={() => setIndex(index - 1)}
+        >
+          <div className="ml-2 flex h-[60px] w-[30px] items-center justify-center rounded-full text-3xl font-bold text-neutral-600 group-hover:bg-neutral-100">
+            <ArrowForwardIosIcon />
+          </div>
+        </button>
+      </div>
+
+      <div className="mt-3 flex flex-row justify-around">
+        {getPreviewImg(previewNumber).map((item, i) => {
+          return (
+            <img
+              className={`h-[50px] w-[50px] cursor-pointer hover:opacity-100 ${
+                i !== previewNumber ? "opacity-40" : ""
+              }`}
+              src={item}
+              onClick={() => {
+                setIndex(index - (i - previewNumber));
+              }}
+              alt="aquarium"
+              key={i}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ImgPicker;
--- frontend/src/pages/user/aquariums/new/new-aquarium.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import WidgetBox from "../../../../components/widget-box/widget-box";
+import ImgPicker from "./components/img-picker/img-picker";
 
 const NewAquarium = () => {
   return (
     <div className="h-full w-full px-5 py-10">
-      <WidgetBox>
-        <div>ss</div>
+      <WidgetBox title="Nowe akwarium">
+        <div className="max-w-[400px]">
+          <ImgPicker onClick={(value) => console.log(value)} />
+        </div>
       </WidgetBox>
     </div>
   );
