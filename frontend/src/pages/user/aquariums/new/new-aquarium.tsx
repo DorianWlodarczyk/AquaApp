51d62782f1befcef03d5e6e139072957a6f76165
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-30 14:22:40 +0200

    Add InputText to new aquarium

--- frontend/src/pages/user/aquariums/new/new-aquarium.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import WidgetBox from "../../../../components/widget-box/widget-box";
 import ImgPicker from "./components/img-picker/img-picker";
-
+import InputText from "../../../../components/input-text/input-text";
+import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
 const NewAquarium = () => {
   return (
     <div className="h-full w-full px-5 py-10">
-      <WidgetBox title="Nowe akwarium">
-        <div className="max-w-[400px]">
-          <ImgPicker onClick={(value) => console.log(value)} />
+      <WidgetBox title="Nowe akwarium" icon={<QueuePlayNextIcon />}>
+        <div className="flex flex-row">
+          <div className="w-[400px]">
+            <ImgPicker
+              onClick={(value) => console.log(`Img Picker ->`, value)}
+            />
+          </div>
+          <InputText
+            label="Nazwa akwarium"
+            helperText="Max 32 znaki"
+            searchIcon
+            clearIcon
+            onChange={(value) => console.log(`Input text ->`, value)}
+          />
         </div>
       </WidgetBox>
     </div>
