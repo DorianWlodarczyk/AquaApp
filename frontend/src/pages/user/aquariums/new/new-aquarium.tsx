339624eea1ae69e2286f84e66568ca7a27f48412
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-30 21:22:00 +0200

    Update new-aquarium.tsx

--- frontend/src/pages/user/aquariums/new/new-aquarium.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WidgetBox from "../../../../components/widget-box/widget-box";
 import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
-import Form, { inputConfig } from "../../../../components/form/form";
+import Form, {
+  getInitState,
+  inputConfig,
+  inputData,
+} from "../../../../components/form/form";
 
 const inputsConfig: inputConfig[] = [
   {
@@ -12,6 +16,7 @@ const inputsConfig: inputConfig[] = [
   {
     type: "TEXT",
     label: "Nazwa",
+    name: "name",
     helperText: "Max 32 znaki",
     className: "lg:col-span-4 col-span-full",
   },
@@ -56,6 +61,8 @@ const inputsConfig: inputConfig[] = [
 ];
 
 const NewAquarium = () => {
+  const [data, setData] = useState<inputData[]>(getInitState(inputsConfig));
+
   return (
     <div className="h-full w-full px-5 py-10">
       <WidgetBox title="Nowe akwarium" icon={<QueuePlayNextIcon />}>
@@ -63,6 +70,8 @@ const NewAquarium = () => {
           <Form
             inputs={inputsConfig}
             className="grid-cols-1 px-3 md:grid-cols-4 lg:grid-cols-6"
+            values={data}
+            onChange={(values) => setData(values)}
           />
         </div>
       </WidgetBox>
