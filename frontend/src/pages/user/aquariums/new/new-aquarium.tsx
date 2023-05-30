de0fac0bf7751625a96a88bf431f79973e2f60e9
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-30 18:15:47 +0200

    add new input config in new aqua page

--- frontend/src/pages/user/aquariums/new/new-aquarium.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium.tsx
@@ -4,16 +4,18 @@ import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
 import Form, { inputConfig } from "../../../../components/form/form";
 
 const inputsConfig: inputConfig[] = [
-  { type: "IMG", className: "row-span-4" },
+  { type: "IMG", className: "row-span-4 lg:col-span-2 col-span-full" },
   {
     type: "TEXT",
     label: "Nazwa",
     helperText: "Max 32 znaki",
-    className: "col-span-2",
+    className: "lg:col-span-4 col-span-full",
   },
+  { type: "LABEL", label: "Wymiary" },
   { type: "TEXT", label: "Długość", helperText: "helper Text" },
   { type: "TEXT", label: "Szerokość", helperText: "helper Text" },
   { type: "TEXT", label: "Wysokość", helperText: "helper Text" },
+  { type: "LABEL", label: "Akcesoria" },
   { type: "TEXT", label: "Grzałka", helperText: "helper Text" },
   { type: "TEXT", label: "Pompa", helperText: "helper Text" },
   { type: "TEXT", label: "Lampa", helperText: "helper Text" },
@@ -24,7 +26,10 @@ const NewAquarium = () => {
     <div className="h-full w-full px-5 py-10">
       <WidgetBox title="Nowe akwarium" icon={<QueuePlayNextIcon />}>
         <div className="flex flex-row">
-          <Form inputs={inputsConfig} className="grid-cols-3" />
+          <Form
+            inputs={inputsConfig}
+            className="grid-cols-1 px-3 md:grid-cols-4 lg:grid-cols-6"
+          />
         </div>
       </WidgetBox>
     </div>
