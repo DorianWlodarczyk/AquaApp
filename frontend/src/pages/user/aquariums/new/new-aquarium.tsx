ca4c4e81b53428e6cf18c13caec709110249bc5d
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-31 00:01:24 +0200

    remove unnecessary import

--- frontend/src/pages/user/aquariums/new/new-aquarium.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import WidgetBox from "../../../../components/widget-box/widget-box";
 import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
 import Form, {
