6cf373d3a338f507bf082974123de97c5eb9cce1
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-30 02:07:20 +0200

    Update widget-box.tsx

--- frontend/src/components/widget-box/widget-box.tsx
+++ frontend/src/components/widget-box/widget-box.tsx
@@ -3,15 +3,17 @@ import IconBox from "./icon-box/icon-box";
 import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
 interface props {
   children: React.ReactNode;
+  title?: string;
 }
 
-const WidgetBox = ({ children }: props) => {
+const WidgetBox = ({ children, title }: props) => {
   return (
-    <div className="relative min-h-[200px] w-full rounded bg-white shadow">
+    <div className="relative w-full rounded bg-white pb-3 shadow">
       <div className="absolute left-[30px] top-[-20px]">
         <IconBox color="GREEN" icon={<QueuePlayNextIcon />} />
       </div>
-      {children}
+      <div className="absolute left-[100px] top-3 text-xl">{title}</div>
+      <div className="w-full pt-[50px]">{children}</div>
     </div>
   );
 };
