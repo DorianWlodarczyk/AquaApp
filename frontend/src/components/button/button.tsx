3aaaac75eb4ef0a24cda668854b7f9aa0224be2c
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-05-31 15:44:14 +0200

    Add onClick to Button

--- frontend/src/components/button/button.tsx
+++ frontend/src/components/button/button.tsx
@@ -8,8 +8,15 @@ interface props {
 }
 
 const Button = ({ text, onClick, icon }: props) => {
+  const onClickHandler = () => {
+    if (onClick) onClick();
+  };
+
   return (
-    <button className="rounded-lg bg-blue-500 p-3 text-base font-semibold text-white">
+    <button
+      className="rounded-lg bg-blue-500 p-3 text-base font-semibold text-white"
+      onClick={onClickHandler}
+    >
       {text}
     </button>
   );
