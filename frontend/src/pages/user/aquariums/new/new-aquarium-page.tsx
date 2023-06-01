d50e06014e0c0b7015bae93d442507006af0dbc1
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-01 03:56:02 +0200

    Update new-aquarium-page.tsx

--- frontend/src/pages/user/aquariums/new/new-aquarium-page.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium-page.tsx
@@ -3,6 +3,7 @@ import WidgetBox from "../../../../components/widget-box/widget-box";
 import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
 import ProgressBar from "./progress-bar/progress-bar";
 import Step1 from "./steps/Step1";
+import Button from "../../../../components/button/button";
 
 export interface inputData {
   value: string;
@@ -13,6 +14,14 @@ export interface inputData {
 const NewAquariumPage = () => {
   const [step, setStep] = useState(0);
   const [inputs, setInputs] = useState<inputData[]>([]);
+
+  const progressLabels = [
+    "Wstęp",
+    "Rozmiar",
+    "Wyposażenie",
+    "Dodatki",
+    "Koniec",
+  ];
 
   const onChange = (value: string, name: string) => {
     const newInputsList = [...inputs];
@@ -32,11 +41,30 @@ const NewAquariumPage = () => {
   return (
     <div className="flex justify-center px-5 py-10">
       <WidgetBox title="Krok 1" icon={<QueuePlayNextIcon />}>
-        <ProgressBar
-          labels={["Wstęp", "Rozmiar", "Wyposażenie", "Dodatki", "Koniec"]}
-          step={step}
-        />
+        <ProgressBar labels={progressLabels} step={step} />
         <div>{step === 0 && <Step1 onChange={onChange} values={inputs} />}</div>
+
+        <div className="flex justify-center">
+          {step === 0 && (
+            <div>
+              <Button text="Dalej" onClick={() => setStep(step + 1)} />
+            </div>
+          )}
+
+          {step > 0 && step < progressLabels.length - 1 && (
+            <div>
+              <Button text="Poprzednie" onClick={() => setStep(step - 1)} />
+              <Button text="Dalej" onClick={() => setStep(step + 1)} />
+            </div>
+          )}
+
+          {step === progressLabels.length - 1 && (
+            <div>
+              <Button text="Poprzednie" onClick={() => setStep(step - 1)} />
+              <Button text="Zakończ" onClick={() => setStep(step + 1)} />
+            </div>
+          )}
+        </div>
       </WidgetBox>
     </div>
   );
