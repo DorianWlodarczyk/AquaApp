3b903dead352348b8994624ff5ee8c9a91d3013a
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-02 14:41:49 +0200

    Add enabled prop to Button

--- frontend/src/components/button/button.tsx
+++ frontend/src/components/button/button.tsx
@@ -5,16 +5,25 @@ interface props {
   text?: string;
   onClick?: () => void;
   icon?: React.ReactElement<SvgIconProps>;
+  enabled?: boolean;
 }
 
-const Button = ({ text, onClick, icon }: props) => {
+const Button = ({ text, onClick, icon, enabled = true }: props) => {
   const onClickHandler = () => {
-    if (onClick) onClick();
+    if (enabled) {
+      if (onClick) {
+        onClick();
+      }
+    }
   };
 
   return (
     <button
-      className="rounded-lg bg-blue-500 p-3 text-base font-semibold text-white"
+      className={`min-w-[100px] rounded-lg  p-3 text-base font-semibold text-white duration-200  ${
+        enabled
+          ? "bg-blue-500 hover:bg-blue-600"
+          : "cursor-default bg-neutral-500 text-neutral-100"
+      }`}
       onClick={onClickHandler}
     >
       {text}
