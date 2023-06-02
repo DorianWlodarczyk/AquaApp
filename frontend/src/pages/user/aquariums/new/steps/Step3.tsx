2c9cb410a980844a994b0e535cabcea91f53e62c
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-02 23:39:01 +0200

    Update Step3.tsx

--- frontend/src/pages/user/aquariums/new/steps/Step3.tsx
+++ frontend/src/pages/user/aquariums/new/steps/Step3.tsx
@@ -1,12 +1,52 @@
 import React from "react";
 import { inputData } from "../../../../../utils/models/input-data";
+import DropDownList, {
+  DropDownOption,
+} from "../../../../../components/drop-down-list/drop-down-list";
 interface props {
   onChange: (value: string, name: string) => void;
   values: inputData[];
 }
 
+const temp: DropDownOption[] = [
+  { name: "A Option1", value: "Option1" },
+  { name: "B Option2", value: "Option2" },
+  { name: "C Option3", value: "Option3" },
+  { name: "AA Option4", value: "Option4" },
+  { name: "AB Option5", value: "Option5" },
+  { name: "AC Option6", value: "Option6" },
+  { name: "CA Option7", value: "Option7" },
+  { name: "CB Option8", value: "Option8" },
+  { name: "CC Option9", value: "Option9" },
+  { name: "AAA Option10", value: "Option10" },
+];
+
 const Step3 = ({ onChange, values }: props) => {
-  return <div>Step3</div>;
+  return (
+    <div className="mt-10 flex w-full justify-center">
+      <div className="flex w-11/12 flex-col items-center justify-around gap-10 lg:w-full lg:flex-row">
+        <DropDownList
+          options={temp}
+          label="Grzałka"
+          onChange={(value) => onChange(value, "heater")}
+          value={values.find((item) => item.name === "heater")?.value || ""}
+        />
+
+        <DropDownList
+          options={temp}
+          label="Pompa"
+          onChange={(value) => onChange(value, "pump")}
+          value={values.find((item) => item.name === "pump")?.value || ""}
+        />
+        <DropDownList
+          options={temp}
+          label="Oświetlenie"
+          onChange={(value) => onChange(value, "lamp")}
+          value={values.find((item) => item.name === "lamp")?.value || ""}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Step3;
