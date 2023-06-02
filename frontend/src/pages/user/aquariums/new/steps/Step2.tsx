552d093f06740f526185ba0a594a36f5fdd6c539
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-02 17:36:44 +0200

    Create Step2 component

---
+++ frontend/src/pages/user/aquariums/new/steps/Step2.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { inputData } from "../../../../../utils/models/input-data";
+import InputText from "../../../../../components/input-text/input-text";
+import AquariumPreview from "../aquarium-preview/aquarium-preview";
+
+interface props {
+  onChange: (value: string, name: string) => void;
+  values: inputData[];
+}
+
+const Step2 = ({ onChange, values }: props) => {
+  return (
+    <div className="mt-5 flex w-full flex-col items-center justify-around lg:flex-row">
+      <div className="flex min-h-[300px] w-[200px] flex-col items-center justify-center">
+        <div className="mb-[100px] w-full text-center text-xl">Podgląd</div>
+        <AquariumPreview values={values} />
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-3 w-full text-center text-xl">
+          Podaj wymiary akwarium
+        </div>
+        <div className="flex h-full flex-col items-center gap-8">
+          <InputText
+            label="Długość"
+            helperText="wartość w [cm]"
+            onChange={(value) => onChange(value, "length")}
+            error={values.find((item) => item.name === "length")?.error}
+            value={values.find((item) => item.name === "length")?.value || ""}
+          />
+          <InputText
+            label="Szerokość"
+            helperText="wartość w [cm]"
+            onChange={(value) => onChange(value, "width")}
+            error={values.find((item) => item.name === "width")?.error}
+            value={values.find((item) => item.name === "width")?.value || ""}
+          />
+          <InputText
+            label="Wysokość"
+            helperText="wartość w [cm]"
+            onChange={(value) => onChange(value, "height")}
+            error={values.find((item) => item.name === "height")?.error}
+            value={values.find((item) => item.name === "height")?.value || ""}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Step2;
