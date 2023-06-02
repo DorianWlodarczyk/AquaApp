665253a5ecedd7d716cb2699e20c8dfea2fbb999
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-02 14:42:47 +0200

    Add input checking to NewAquariumPage

--- frontend/src/pages/user/aquariums/new/new-aquarium-page.tsx
+++ frontend/src/pages/user/aquariums/new/new-aquarium-page.tsx
@@ -4,16 +4,19 @@ import QueuePlayNextIcon from "@mui/icons-material/QueuePlayNext";
 import ProgressBar from "./progress-bar/progress-bar";
 import Step1 from "./steps/Step1";
 import Button from "../../../../components/button/button";
+import { maxNameLength } from "../../../../utils/regex/text-input.regex";
 
 export interface inputData {
   value: string;
   error: boolean;
   name: string;
+  pattern?: RegExp;
 }
 
 const NewAquariumPage = () => {
   const [step, setStep] = useState(0);
   const [inputs, setInputs] = useState<inputData[]>([]);
+  const [enabledButton, setEnabledButton] = useState(false);
 
   const progressLabels = [
     "Wstęp",
@@ -23,18 +26,45 @@ const NewAquariumPage = () => {
     "Koniec",
   ];
 
+  const inputsOnPage = [
+    ["name", "imgID"],
+    ["width", "height", "length"],
+  ];
+
   const onChange = (value: string, name: string) => {
     const newInputsList = [...inputs];
 
-    newInputsList.find((item) => item.name === name)!.value = value;
+    const input = newInputsList.find((item) => item.name === name);
 
+    if (!input) return;
+
+    if (input.pattern) input.error = !input.pattern.test(value);
+
+    input.value = value;
     setInputs(newInputsList);
+
+    checkInputs();
+  };
+
+  const checkInputs = () => {
+    let isOk = true;
+    for (let i = 0; i <= step; i++) {
+      for (let item of inputsOnPage[i]) {
+        isOk =
+          (isOk && !inputs.find((element) => element.name === item)?.error) ??
+          false;
+      }
+    }
+    setEnabledButton(isOk);
   };
 
   useEffect(() => {
     setInputs([
-      { name: "name", value: "", error: false },
+      { name: "name", value: "", error: true, pattern: maxNameLength },
       { name: "imgID", value: "1", error: false },
+      { name: "width", value: "", error: true },
+      { name: "height", value: "", error: true },
+      { name: "length", value: "", error: true },
     ]);
   }, []);
 
@@ -47,21 +77,33 @@ const NewAquariumPage = () => {
         <div className="flex justify-center">
           {step === 0 && (
             <div>
-              <Button text="Dalej" onClick={() => setStep(step + 1)} />
+              <Button
+                text="Dalej"
+                enabled={enabledButton}
+                onClick={() => setStep(step + 1)}
+              />
             </div>
           )}
 
           {step > 0 && step < progressLabels.length - 1 && (
             <div>
               <Button text="Poprzednie" onClick={() => setStep(step - 1)} />
-              <Button text="Dalej" onClick={() => setStep(step + 1)} />
+              <Button
+                text="Dalej"
+                enabled={enabledButton}
+                onClick={() => setStep(step + 1)}
+              />
             </div>
           )}
 
           {step === progressLabels.length - 1 && (
             <div>
               <Button text="Poprzednie" onClick={() => setStep(step - 1)} />
-              <Button text="Zakończ" onClick={() => setStep(step + 1)} />
+              <Button
+                text="Zakończ"
+                enabled={enabledButton}
+                onClick={() => setStep(step)}
+              />
             </div>
           )}
         </div>
