73c15cda852e1ebb36ce64243da82e8cc897f786
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-05 03:16:56 +0200

    feat(Step3): Add hint

--- frontend/src/pages/user/aquariums/new/steps/Step3.tsx
+++ frontend/src/pages/user/aquariums/new/steps/Step3.tsx
@@ -8,12 +8,24 @@ interface props {
   onChange: (value: string, name: string) => void;
   values: inputData[];
   accessories: AccessoriesData;
+  volume: number;
 }
 
-const Step3 = ({ onChange, values, accessories }: props) => {
+const Step3 = ({ onChange, values, accessories, volume }: props) => {
   const [heaters, setHeaters] = useState<DropDownOption[]>([]);
   const [lamps, setLamps] = useState<DropDownOption[]>([]);
   const [pumps, setPumps] = useState<DropDownOption[]>([]);
+
+  const showHeatersHint = () => {
+    const id = values.find((item) => item.name === "heater")?.value;
+    const maxVolume = accessories.heaters.find(
+      (item) => item.id === id
+    )?.maxCapacity;
+
+    console.log(id, maxVolume, volume);
+    // if()
+    return <>{values.find((item) => item.name === "heater")?.value}</>;
+  };
 
   useEffect(() => {
     setHeaters(
@@ -47,28 +59,36 @@ const Step3 = ({ onChange, values, accessories }: props) => {
   return (
     <div className="mt-10 flex w-full justify-center">
       <div className="flex w-11/12 flex-col items-center justify-around gap-16 lg:w-9/12 lg:flex-row">
-        <DropDownList
-          options={heaters}
-          sort
-          label="Grzałka"
-          onChange={(value) => onChange(value, "heater")}
-          value={values.find((item) => item.name === "heater")?.value || ""}
-        />
+        <div className="w-full">
+          <DropDownList
+            options={heaters}
+            sort
+            label="Grzałka"
+            onChange={(value) => onChange(value, "heater")}
+            value={values.find((item) => item.name === "heater")?.value || ""}
+          />
+          {showHeatersHint()}
+        </div>
 
-        <DropDownList
-          options={lamps}
-          label="Pompa"
-          sort
-          onChange={(value) => onChange(value, "pump")}
-          value={values.find((item) => item.name === "pump")?.value || ""}
-        />
-        <DropDownList
-          options={pumps}
-          label="Oświetlenie"
-          sort
-          onChange={(value) => onChange(value, "lamp")}
-          value={values.find((item) => item.name === "lamp")?.value || ""}
-        />
+        <div className="w-full">
+          <DropDownList
+            options={lamps}
+            label="Pompa"
+            sort
+            onChange={(value) => onChange(value, "pump")}
+            value={values.find((item) => item.name === "pump")?.value || ""}
+          />
+        </div>
+
+        <div className="w-full">
+          <DropDownList
+            options={pumps}
+            label="Oświetlenie"
+            sort
+            onChange={(value) => onChange(value, "lamp")}
+            value={values.find((item) => item.name === "lamp")?.value || ""}
+          />
+        </div>
       </div>
     </div>
   );
