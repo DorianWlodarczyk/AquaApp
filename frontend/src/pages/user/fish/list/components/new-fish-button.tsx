31a9192a8ec4a9610d3f5e20e16f597151f31405
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-07 01:43:30 +0200

    feat(NewFishButton): Create NewFishButton component

---
+++ frontend/src/pages/user/fish/list/components/new-fish-button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NewFishButton = () => {
+  return (
+    <Link to={"new"}>
+      <div className="group flex h-[130px] cursor-pointer select-none flex-row items-center justify-center rounded-3xl border-[4px] border-dashed border-neutral-400 hover:border-neutral-500">
+        <div className="font-bold text-neutral-400 group-hover:text-neutral-500">
+          Dodaj nową rybę
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default NewFishButton;
