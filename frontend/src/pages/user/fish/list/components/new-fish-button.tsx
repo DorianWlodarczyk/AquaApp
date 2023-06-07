618c5505f10daf7f283d3faf604f9aefd8f6c2c1
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-07 02:24:56 +0200

    feat(NewFishButton): Update button link

--- frontend/src/pages/user/fish/list/components/new-fish-button.tsx
+++ frontend/src/pages/user/fish/list/components/new-fish-button.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NewFishButton = () => {
+interface props {
+  aquariumID?: string;
+}
+
+const NewFishButton = ({ aquariumID = "" }: props) => {
   return (
-    <Link to={"new"}>
+    <Link to={`/aqua/${aquariumID}/fish/new`}>
       <div className="group flex h-[130px] cursor-pointer select-none flex-row items-center justify-center rounded-3xl border-[4px] border-dashed border-neutral-400 hover:border-neutral-500">
         <div className="font-bold text-neutral-400 group-hover:text-neutral-500">
           Dodaj nową rybę
