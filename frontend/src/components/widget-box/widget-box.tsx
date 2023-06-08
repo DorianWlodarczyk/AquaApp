0f3806fc51cf5231f555582936cb17418908f8ac
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-06-09 00:02:02 +0200

    chore(IconBox): create enum Color

--- frontend/src/components/widget-box/widget-box.tsx
+++ frontend/src/components/widget-box/widget-box.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import IconBox from "./icon-box/icon-box";
+import IconBox, { Color } from "./icon-box/icon-box";
 import { SvgIconProps } from "@mui/material";
 interface props {
   children: React.ReactNode;
   title?: string;
   icon?: React.ReactElement<SvgIconProps>;
   className?: string;
+  color?: Color;
 }
 
-const WidgetBox = ({ children, title, icon, className }: props) => {
+const WidgetBox = ({ children, title, icon, className, color }: props) => {
   return (
     <div
       className={`relative w-full rounded bg-white pb-3 shadow ${
@@ -16,7 +17,7 @@ const WidgetBox = ({ children, title, icon, className }: props) => {
       }`}
     >
       <div className="absolute left-[30px] top-[-20px]">
-        {icon && <IconBox color="GREEN" icon={icon} />}
+        {(icon || color) && <IconBox color={color} icon={icon} />}
       </div>
       <div className="absolute left-[100px] top-3 text-xl">{title}</div>
       <div className="w-full pt-[50px]">{children}</div>
