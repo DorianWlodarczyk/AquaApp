2425de28128a6ee6a53a935e3655b19859900c3d
Czarek Karczewski <keracz1997@gmail.com>
2023-06-08 13:11:45 +0200

    set urls configuration / delete views.py

--- backend/aqua/views.py
+++ backend/aqua/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from datetime import datetime
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
