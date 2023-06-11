7c6e2d326f0bc723cdb58080788159cc137ec2f2
Czarek Karczewski <keracz1997@gmail.com>
2023-06-11 14:41:32 +0200

    update admin file, add personalize display model

--- backend/aqua/admin.py
+++ backend/aqua/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(AquaAccount)
-admin.site.register(AquaHistory)
+@admin.register(AquaAccount)
+class AquaAccountAdmin(admin.ModelAdmin):
+  list_display = ('id_aqua_account', 'user_mail', 'user_token', 'is_admin')
+  
+@admin.register(AquaHistory)
+class AquaHistoryAdmin(admin.ModelAdmin):
+  list_display = ('id_aqua_history', 'id_aqua_account', 'log_info')
