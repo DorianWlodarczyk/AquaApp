a5b8320bc82a9fc7248940893a981a4d77e2c571
Czarek Karczewski <keracz1997@gmail.com>
2023-06-11 14:41:51 +0200

    add testing endpoint

--- backend/aqua/views.py
+++ backend/aqua/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
-from aqua.models import AquaHistory
+from aqua.models import AquaHistory, AquaAccount
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def log(user_id, message):
   message_log = AquaHistory(id_aqua_account= user_id, log_info= message)
   message_log.save()
-
+  
+@csrf_exempt
+@require_http_methods(["POST"])
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+  user_id = request.POST.get('user')
+  # user_id = "hwdp2137BDSM420jp2w13lk1_p0l@k69"
+  id_aqua_account = AquaAccount.objects.get(user_token = user_id)
+  
+  if not user_id:
+    print("podaj user id")
+    print(user_id)
+    return HttpResponse("chujowy use id")
+  else: 
+    print(user_id)
+    log(user_id=id_aqua_account, message="nie sztuka uciec gdy w dupie sztuciec")
+    return HttpResponse("user id ok")
+    
