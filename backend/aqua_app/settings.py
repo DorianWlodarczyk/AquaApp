d4aeda83444f4f2945a6cd90ce563372929dcbb0
DorianWlodarczyk <215365@edu.p.lodz.pl>
2023-06-11 11:34:16 +0200

    Added firebase_admin library to requirements file

--- backend/aqua_app/settings.py
+++ backend/aqua_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,6 +82,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 
 # Password validation
