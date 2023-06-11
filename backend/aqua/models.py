553be3baf5fba921630504a7ddccb5a24a9e1aa7
Czarek Karczewski <keracz1997@gmail.com>
2023-06-11 13:20:08 +0200

    change primary key file type from integer on BigAutoField

--- backend/aqua/models.py
+++ backend/aqua/models.py
@@ -9,9 +9,9 @@ from django.db import models
 
 
 class AquaAccount(models.Model):
-    id_aqua_account = models.IntegerField(primary_key=True)
+    id_aqua_account = models.BigAutoField(primary_key=True)
     user_mail = models.CharField(max_length=60)
-    user_token = models.IntegerField(blank=True, null=True)
+    user_token = models.CharField(max_length=256, blank=True, null=True)
     is_admin = models.BooleanField(blank=True, null=True)
 
     class Meta:
@@ -20,7 +20,7 @@ class AquaAccount(models.Model):
 
 
 class AquaDecorator(models.Model):
-    id_aqua_decorator = models.IntegerField(primary_key=True)
+    id_aqua_decorator = models.BigAutoField(primary_key=True)
     id_ground = models.ForeignKey('Ground', models.PROTECT, db_column='id_ground')
     id_plant = models.ForeignKey('Plant', models.PROTECT, db_column='id_plant')
     id_asset = models.ForeignKey('Asset', models.PROTECT, db_column='id_asset')
@@ -31,7 +31,7 @@ class AquaDecorator(models.Model):
 
 
 class AquaHistory(models.Model):
-    id_aqua_history = models.IntegerField(primary_key=True)
+    id_aqua_history = models.BigAutoField(primary_key=True)
     id_aqua_account = models.ForeignKey(AquaAccount, models.CASCADE, db_column='id_aqua_account', blank=True, null=True)
     log_info = models.CharField(max_length=500, blank=True, null=True)
 
@@ -41,7 +41,7 @@ class AquaHistory(models.Model):
 
 
 class AquaLife(models.Model):
-    id_aqua_life_fish = models.IntegerField(primary_key=True)
+    id_aqua_life_fish = models.BigAutoField(primary_key=True)
     id_fish = models.ForeignKey('Fish', models.PROTECT, db_column='id_fish')
     id_fish_life_time = models.DateField(blank=True, null=True)
     id_tank_object = models.ForeignKey('TankObject', models.CASCADE, db_column='id_tank_object', blank=True, null=True)
@@ -53,7 +53,7 @@ class AquaLife(models.Model):
 
 
 class AquaMaker(models.Model):
-    id_aqua_maker = models.IntegerField(primary_key=True)
+    id_aqua_maker = models.BigAutoField(primary_key=True)
     id_aquarium_tank = models.ForeignKey('AquariumTank', models.CASCADE, db_column='id_aquarium_tank')
     id_heater = models.ForeignKey('Heater', models.PROTECT, db_column='id_heater')
     id_pump = models.ForeignKey('Pump', models.PROTECT, db_column='id_pump')
@@ -65,7 +65,7 @@ class AquaMaker(models.Model):
 
 
 class AquariumTank(models.Model):
-    id_aquarium_tank = models.IntegerField(primary_key=True)
+    id_aquarium_tank = models.BigAutoField(primary_key=True)
     size_width = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
     size_height = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
     size_lenght = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
@@ -76,7 +76,7 @@ class AquariumTank(models.Model):
 
 
 class Asset(models.Model):
-    id_asset = models.IntegerField(primary_key=True)
+    id_asset = models.BigAutoField(primary_key=True)
     asset_name = models.CharField(max_length=45, blank=True, null=True)
     asset_size_flag = models.IntegerField(blank=True, null=True)
 
@@ -85,7 +85,7 @@ class Asset(models.Model):
         db_table = 'asset'
 
 class Fish(models.Model):
-    id_fish = models.IntegerField(primary_key=True)
+    id_fish = models.BigAutoField(primary_key=True)
     fish_name = models.CharField(max_length=45, blank=True, null=True)
     fish_size = models.IntegerField(blank=True, null=True)
     min_ph = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
@@ -101,9 +101,9 @@ class Fish(models.Model):
 
 
 class FishConflict(models.Model):
-    id_fish_conflict = models.IntegerField(primary_key=True)
+    id_fish_conflict = models.BigAutoField(primary_key=True)
     id_first_fish = models.ForeignKey(Fish, models.PROTECT, db_column='id_first_fish', blank=True, null=True)
-    id_second_fish = models.ForeignKey(Fish, models.PROTECT, db_column='id_second_fish', blank=True, null=True)
+    id_second_fish = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = True
@@ -111,7 +111,7 @@ class FishConflict(models.Model):
 
 
 class Ground(models.Model):
-    id_ground = models.IntegerField(primary_key=True)
+    id_ground = models.BigAutoField(primary_key=True)
     ground_name = models.CharField(max_length=45, blank=True, null=True)
     ground_size = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
 
@@ -121,7 +121,7 @@ class Ground(models.Model):
 
 
 class Heater(models.Model):
-    id_heater = models.IntegerField(primary_key=True)
+    id_heater = models.BigAutoField(primary_key=True)
     heater_name = models.CharField(max_length=45, blank=True, null=True)
     heater_min_power = models.IntegerField(blank=True, null=True)
     heater_max_power = models.IntegerField(blank=True, null=True)
@@ -134,7 +134,7 @@ class Heater(models.Model):
 
 
 class Lamp(models.Model):
-    id_lamp = models.IntegerField(primary_key=True)
+    id_lamp = models.BigAutoField(primary_key=True)
     lamp_name = models.CharField(max_length=60, blank=True, null=True)
     power = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
 
@@ -144,7 +144,7 @@ class Lamp(models.Model):
 
 
 class Plant(models.Model):
-    id_plant = models.IntegerField(primary_key=True)
+    id_plant = models.BigAutoField(primary_key=True)
     plant_name = models.CharField(max_length=45, blank=True, null=True)
 
     class Meta:
@@ -153,7 +153,7 @@ class Plant(models.Model):
 
 
 class Pump(models.Model):
-    id_pump = models.IntegerField(primary_key=True)
+    id_pump = models.BigAutoField(primary_key=True)
     pump_name = models.CharField(max_length=60, blank=True, null=True)
     power = models.IntegerField(blank=True, null=True)
     pump_capacity = models.IntegerField(blank=True, null=True)
@@ -165,7 +165,7 @@ class Pump(models.Model):
 
 
 class TankObject(models.Model):
-    id_tank_object = models.IntegerField(primary_key=True)
+    id_tank_object = models.BigAutoField(primary_key=True)
     id_aqua_decorator = models.ForeignKey(AquaDecorator, models.PROTECT, db_column='id_aqua_decorator')
     id_aqua_maker = models.ForeignKey(AquaMaker, models.PROTECT, db_column='id_aqua_maker')
     id_aqua_account = models.ForeignKey(AquaAccount, models.CASCADE, db_column='id_aqua_account')
