945a432db059710319ddad123f9343fcc7bc6c56
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-09-11 15:58:41 +0200

    endpoint

--- frontend/src/utils/api/api.service.ts
+++ frontend/src/utils/api/api.service.ts
@@ -1,24 +1,161 @@
 import axios, { AxiosResponse } from "axios";
+import { getToken } from "../firebase";
 
-class ApiService {
+class Api {
   private static API_ROOT =
-    process.env.REACT_APP_API_ROOT || "http://localhost:8080";
+    process.env.REACT_APP_API_ROOT ||
+    "https://aqua-app-backend-develop.onrender.com/aqua";
+
+  private static token = "";
+
+  private static debugMode = false;
+
+  private static getDelay() {
+    return Math.random() * 0 + 0;
+  }
+
+  private static async getTokenID(): Promise<string> {
+    return getToken()
+      .then((val) => {
+        return val;
+      })
+      .catch((e) => {
+        return " ";
+      });
+  }
 
   protected static async get<T>(
     link: string,
     headers?: {}
   ): Promise<T | undefined> {
+    const newHeaders = {
+      ...headers,
+      token: `${await this.getTokenID()}`,
+    };
+
+    if (this.debugMode) {
+      await new Promise((r) => setTimeout(r, this.getDelay()));
+
+      console.log(`GET - Link -> ${this.API_ROOT}/${link}`, `headers ->`, {
+        headers: newHeaders,
+      });
+      return;
+    }
+
     try {
       const res: AxiosResponse<T, any> = await axios.get<T>(
         `${this.API_ROOT}/${link}`,
-        { headers: headers }
+        {
+          headers: newHeaders,
+        }
       );
 
       return res.data;
-    } catch {
+    } catch (e) {
+      throw e;
       //Obsługa błędów
+    }
+  }
+
+  protected static async post<T>(
+    link: string,
+    body: any,
+    headers?: {}
+  ): Promise<T | undefined> {
+    const newHeaders = {
+      ...headers,
+      token: `${await this.getTokenID()}`,
+    };
+
+    if (this.debugMode) {
+      await new Promise((r) => setTimeout(r, this.getDelay()));
+      console.log(
+        `POST - Link -> ${this.API_ROOT}/${link}`,
+        ` body -> `,
+        body,
+        ` headers -> `,
+        { headers: newHeaders }
+      );
+      return;
+    }
+
+    try {
+      const res: AxiosResponse<T, any> = await axios.post(
+        `${this.API_ROOT}/${link}`,
+        body,
+        { headers: newHeaders }
+      );
+
+      return res.data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  protected static async put<T>(
+    link: string,
+    body: any,
+    headers?: {}
+  ): Promise<T | undefined> {
+    const newHeaders = {
+      ...headers,
+      token: `${await this.getTokenID()}`,
+    };
+
+    if (this.debugMode) {
+      await new Promise((r) => setTimeout(r, this.getDelay()));
+
+      console.log(
+        `PUT - Link -> ${this.API_ROOT}/${link}`,
+        ` body -> `,
+        body,
+        ` headers -> `,
+        { headers: newHeaders }
+      );
+      return;
+    }
+
+    try {
+      const res: AxiosResponse<T, any> = await axios.put(
+        `${this.API_ROOT}/${link}`,
+        body,
+        { headers: newHeaders }
+      );
+
+      return res.data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  protected static async delete<T>(
+    link: string,
+    headers?: {}
+  ): Promise<T | undefined> {
+    const newHeaders = {
+      ...headers,
+      token: `${await this.getTokenID()}`,
+    };
+
+    if (this.debugMode) {
+      await new Promise((r) => setTimeout(r, this.getDelay()));
+
+      console.log(`DELETE - Link -> ${this.API_ROOT}/${link}`, `headers ->`, {
+        headers: newHeaders,
+      });
+      return;
+    }
+    try {
+      const res: AxiosResponse<T, any> = await axios.delete(
+        `${this.API_ROOT}/${link}`,
+        { headers: newHeaders }
+      );
+
+      return res.data;
+    } catch (e) {
+      throw e;
     }
   }
 }
 
-export default ApiService;
+export default Api;
