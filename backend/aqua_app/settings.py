6dcaffe1ca2ec29fad769a6210808e2dbe15b07c
DorianWlodarczyk <215365@edu.p.lodz.pl>
2023-09-12 20:24:06 +0200

    Fixed cors once again

--- backend/aqua_app/settings.py
+++ backend/aqua_app/settings.py
@@ -45,17 +45,7 @@ CORS_ALLOWED_METHODS = [
     'OPTIONS'
 ]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
+CORS_ALLOW_HEADERS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
 
